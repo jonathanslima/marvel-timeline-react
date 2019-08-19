25ad22aa920e9a9b22c8cca5ea39461be7b35227
Lima <jonathan.da.s.lima@accenture.com>
2019-08-18 23:15:01 -0300

    deploy heroku

--- src/components/guideline/index.js
+++ src/components/guideline/index.js
@@ -11,10 +11,28 @@ export default class guideline extends Component {
 				<div className="guide-line-vert-right grow-line-bottom-right"></div>
 				<div className="guide-line-show show-opac-line"></div>
 				<div className="guide-line-vert-left grow-line-bottom-left"></div>
+
 				<div className="guide-line-hor grow-line-right-2"></div>
 				<div className="guide-line-vert-right grow-line-bottom-right-2"></div>
 				<div className="guide-line-show show-opac-line-2"></div>
 				<div className="guide-line-vert-left grow-line-bottom-left-2"></div>
+
+				<div className="guide-line-hor grow-line-right-3"></div>
+				<div className="guide-line-vert-right grow-line-bottom-right-3"></div>
+				<div className="guide-line-show show-opac-line-3"></div>
+				<div className="guide-line-vert-left grow-line-bottom-left-3"></div>
+
+				<div className="guide-line-hor grow-line-right-4"></div>
+				<div className="guide-line-vert-right grow-line-bottom-right-4"></div>
+				<div className="guide-line-show show-opac-line-4"></div>
+				<div className="guide-line-vert-left grow-line-bottom-left-4"></div>
+
+				<div className="guide-line-hor grow-line-right-5"></div>
+				<div className="guide-line-vert-right grow-line-bottom-right-5"></div>
+				<div className="guide-line-show show-opac-line-5"></div>
+				<div className="guide-line-vert-left grow-line-bottom-left-5"></div>
+
+				<div className="guide-line-hor grow-line-right-6"></div>
 
 			</div>
 		);
